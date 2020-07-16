38ea49084eec115f3a766dedbcc85b7951a6ec09
Ana Soles <ana.soles.valdivia@gmail.com>
2020-07-16 22:44:35 +0200

    read dictionary web page

--- dk_learn_new_words_gh.py
+++ dk_learn_new_words_gh.py
@@ -1,3 +1,5 @@
+import string
+
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
@@ -12,9 +14,39 @@ fname = input("Enter file name: ")
 fhand = open(fname, encoding="utf8")
 
 
-word = list()
+SYMBOLS = '{}()[].,:;+-*/&|<>=~$1234567890?'
+words = list()
 for line in fhand:
+	for s in SYMBOLS:
+		line = line.replace(s," ")
+	print(line)
 	line=line.split()
-	word= word + line
-#print(line)
-print(word)
+	
+	words= words + line
+
+
+#print(words)
+	for word in words :
+		print(word)
+		url = "https://ordnet.dk/ddo/ordbog?query=%s" % (urllib.parse.quote(word)) # input('Enter URL- ')
+		html = urllib.request.urlopen(url, context=ctx).read()
+		
+		
+		soup = BeautifulSoup(html, 'html.parser')
+		#print(soup)
+		# # Retrieve all of the anchor tags
+		tags = soup('div')
+		
+		for tag in tags:
+			tag_class = tag.get('class', [])
+			if "definitionBoxTop" in tag_class:
+				# print(tag_class)
+				children = tag.findChildren("span" , recursive=False)
+				for child in children: 
+					span_class = child.get('class', [])
+					if "tekstmedium" in span_class:
+						print(child.text)
+
+
+
+
